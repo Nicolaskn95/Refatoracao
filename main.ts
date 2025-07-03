425277836fee1b6b59f6d4a8a2de57f5e26c59e2
Nicolaskn95 <nicolasjap@hotmail.com>
2025-07-03 00:24:19 -0300

    Refactor amountFor function to use playsFor helper for improved clarity

--- main.ts
+++ main.ts
@@ -48,32 +48,32 @@ export function statement(invoice: invoice, plays: Record<string, play>) {
   ) {
     return plays[aPerfomance.playID];
   }
-}
 
-function amountFor(
-  aPerformance: { playID: string; audience: number },
-  play: any,
-): number {
-  let result = 0;
-  switch (play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`);
+  function amountFor(
+    aPerformance: { playID: string; audience: number },
+    play: any,
+  ): number {
+    let result = 0;
+    switch (playsFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${playsFor(aPerformance).type}`);
+    }
+
+    return result;
   }
-
-  return result;
 }
 
 console.log(statement(invoices[0], plays));
