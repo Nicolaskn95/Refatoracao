0681d17bea61144c9beb290e3684a2b39053be9f
Nicolaskn95 <nicolasjap@hotmail.com>
2025-07-02 23:20:23 -0300

    Refactor statement function to use amountFor helper; improve readability

--- main.ts
+++ main.ts
@@ -25,25 +25,7 @@ export function statement(invoice: invoice, plays: Record<string, play>) {
 
   for (const perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
-
-    switch (play.type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
+    const thisAmount = amountFor(perf, plays); // change the switch to a function call
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -61,6 +43,34 @@ export function statement(invoice: invoice, plays: Record<string, play>) {
   result += `You earned ${volumeCredits} credits\n`;
 
   return result;
+}
+
+function amountFor(
+  perf: { playID: string; audience: number },
+  plays: Record<string, play>,
+): number {
+  const play = plays[perf.playID];
+  let thisAmount = 0;
+
+  switch (play.type) {
+    case "tragedy":
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case "comedy":
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 10000 + 500 * (perf.audience - 20);
+      }
+      thisAmount += 300 * perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+
+  return thisAmount;
 }
 
 console.log(statement(invoices[0], plays));
